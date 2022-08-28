bb98e2f48ba0ec811db11ab2b10efc5c2056f4ce
lei1996 <617218165@qq.com>
2022-08-28 12:28:36 +0000

    update.

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,7 @@
+import {ShenZhenStockClient} from './shenzhenStockClient';
+
 console.log('This is what would run if your app gets started.');
+
+const client = new ShenZhenStockClient();
+
+client.fetchConvertibleBondData().subscribe(console.log);
